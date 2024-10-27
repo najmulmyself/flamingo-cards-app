989df466b664f2f4f67dc0852f697701f54f67f9
najmulmyself <najmul.myself@gmail.com>
2024-10-28 02:29:27 +0600

    firebase connected

--- lib/features/screens/choose_pack_screen.dart
+++ lib/features/screens/choose_pack_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flamingo_app/core/widget/common_appbar.dart';
 import 'package:flamingo_app/features/screens/play_games_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,57 +16,105 @@ class ChoosePacksScreen extends StatelessWidget {
             const CommonSliverAppBar(),
             SliverPadding(
               padding: const EdgeInsets.all(16.0),
-              sliver: SliverList(
-                delegate: SliverChildListDelegate([
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => const PlayGamesScreen()));
-                    },
-                    child: const PackCard(
-                      title: 'Getting Closer',
-                      description:
-                          'Say goodbye to basic small talks and embrace meaningful conversations',
-                      color: Color(0xFF5FBDFF),
-                      icon: '👥',
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  const PackCard(
-                    title: 'Hot & Spicy',
-                    description:
-                        'Turn up the heat, explore secret fantasies, and ignite the passion',
-                    color: Color(0xFFFF69B4),
-                    icon: '🔥',
-                  ),
-                  const SizedBox(height: 16),
-                  const PackCard(
-                    title: 'Love Talks',
-                    description:
-                        'Your guide to a happier relationship - speak truth, share past and future plans',
-                    color: Color(0xFFFF69B4),
-                    icon: '❤️',
-                  ),
-                  const SizedBox(height: 16),
-                  const PackCard(
-                    title: 'Deep Dive!',
-                    description:
-                        'Deep conversation topics to get you thinking and talking!',
-                    color: Color(0xFF4169E1),
-                    icon: '🤔',
-                  ),
-                  const SizedBox(height: 16),
-                  const PackCard(
-                    title: 'Would you rather',
-                    description:
-                        'Dive into fun dilemmas - good luck making those choices!',
-                    color: Color(0xFF8A2BE2),
-                    icon: '🤷',
-                  ),
-                ]),
-              ),
+              sliver: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('party-category')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const SliverToBoxAdapter(
+                        child: Center(child: CircularProgressIndicator()),
+                      );
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return const SliverToBoxAdapter(
+                        child: Center(
+                            child: Text(
+                          'No packs available',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                      );
+                    }
+
+                    final packs = snapshot.data!.docs;
+                    print("packs: ${packs.first.id}");
+
+                    return SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final pack = packs[index];
+                          return GestureDetector(
+                            onTap: () {
+                              // print(pack);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) =>  PlayGamesScreen(id: pack.id,)));
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: PackCard(
+                                title: pack['title'] ?? "No Title",
+                                description: pack['desc'] ?? "No Description",
+                                color: const Color(0xFF5FBDFF),
+                                icon: '👥',
+                              ),
+                            ),
+                          );
+                        },
+                        childCount: packs.length,
+                      ),
+
+                      // GestureDetector(
+                      //   onTap: () {
+                      //     Navigator.push(
+                      //         context,
+                      //         MaterialPageRoute(
+                      //             builder: (_) => const PlayGamesScreen()));
+                      //   },
+                      //   child: const PackCard(
+                      //     title: 'Getting Closer',
+                      //     description:
+                      //         'Say goodbye to basic small talks and embrace meaningful conversations',
+                      //     color: Color(0xFF5FBDFF),
+                      //     icon: '👥',
+                      //   ),
+                      // ),
+                      // const SizedBox(height: 16),
+                      // const PackCard(
+                      //   title: 'Hot & Spicy',
+                      //   description:
+                      //       'Turn up the heat, explore secret fantasies, and ignite the passion',
+                      //   color: Color(0xFFFF69B4),
+                      //   icon: '🔥',
+                      // ),
+                      // const SizedBox(height: 16),
+                      // const PackCard(
+                      //   title: 'Love Talks',
+                      //   description:
+                      //       'Your guide to a happier relationship - speak truth, share past and future plans',
+                      //   color: Color(0xFFFF69B4),
+                      //   icon: '❤️',
+                      // ),
+                      // const SizedBox(height: 16),
+                      // const PackCard(
+                      //   title: 'Deep Dive!',
+                      //   description:
+                      //       'Deep conversation topics to get you thinking and talking!',
+                      //   color: Color(0xFF4169E1),
+                      //   icon: '🤔',
+                      // ),
+                      // const SizedBox(height: 16),
+                      // const PackCard(
+                      //   title: 'Would you rather',
+                      //   description:
+                      //       'Dive into fun dilemmas - good luck making those choices!',
+                      //   color: Color(0xFF8A2BE2),
+                      //   icon: '🤷',
+                      // ),
+                      // ),
+                    );
+                  }),
             ),
           ],
         ),
--- lib/features/screens/play_games_screen.dart
+++ lib/features/screens/play_games_screen.dart
@@ -1,184 +1,223 @@
 import 'package:appinio_swiper/appinio_swiper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flamingo_app/features/screens/subsciption_screen.dart';
 import 'package:flutter/material.dart';
 
 class PlayGamesScreen extends StatelessWidget {
-  const PlayGamesScreen({super.key});
+  const PlayGamesScreen({required this.id, super.key});
+
+  final String id;
+
+  Future<List<Map<String, dynamic>>> fetchQuestions() async {
+    final questionsSnapshot = await FirebaseFirestore.instance
+        .collection('party-category')
+        .doc(id)
+        .collection('questions')
+        .get();
+
+    return questionsSnapshot.docs.map((doc) => doc.data()).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0A2647),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: AppinioSwiper(
-                cardCount: 5,
-                loop: false,
-                allowUnlimitedUnSwipe: false,
-                onSwipeEnd: (index, direction, hello) {
-                  print("in every swap");
-                },
-                onEnd: () {
-                  print("when cards end");
-                },
-                // itemWidth: 300,
-                // layout: SwiperLayout.STACK,
-                cardBuilder: (context, index) {
-                  return Stack(
-                    children: [
-                      // Positioned(
-                      //   top: 16,
-                      //   left: 16,
-                      //   child: Container(
-                      //     width: 40,
-                      //     height: 20,
-                      //     decoration: BoxDecoration(
-                      //       color: Colors.white.withOpacity(0.3),
-                      //       borderRadius: BorderRadius.circular(10),
-                      //     ),
-                      //   ),
-                      // ),
-                      Center(
-                        child: Container(
-                          width: 350,
-                          height: 600,
-                          margin: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            border:
-                                Border.all(color: Colors.lightBlue, width: 4),
-                          ),
-                          child: const Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(40),
-                                // child: Image.asset('assets/swipe_icon.png', width: 100, height: 100),
-                                child: Icon(
-                                  Icons.swipe,
-                                  size: 100,
-                                  color: Colors.lightBlue,
+        child: FutureBuilder(
+            future: fetchQuestions(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return const Center(
+                    child: Text(
+                  "Error loading questions",
+                  style: TextStyle(color: Colors.white),
+                ));
+              }
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                    child: Text("No questions available",
+                        style: TextStyle(
+                          color: Colors.white,
+                        )));
+              }
+
+              final questions = snapshot.data!;
+
+              return Column(
+                children: [
+                  Expanded(
+                    child: AppinioSwiper(
+                      cardCount: questions.length,
+                      loop: false,
+                      allowUnlimitedUnSwipe: false,
+                      onSwipeEnd: (index, direction, hello) {
+                        print("in every swap");
+                      },
+                      onEnd: () {
+                        print("when cards end");
+                      },
+                      // itemWidth: 300,
+                      // layout: SwiperLayout.STACK,
+                      cardBuilder: (context, index) {
+                        final question = questions[index];
+                        return Stack(
+                          children: [
+                            // Positioned(
+                            //   top: 16,
+                            //   left: 16,
+                            //   child: Container(
+                            //     width: 40,
+                            //     height: 20,
+                            //     decoration: BoxDecoration(
+                            //       color: Colors.white.withOpacity(0.3),
+                            //       borderRadius: BorderRadius.circular(10),
+                            //     ),
+                            //   ),
+                            // ),
+                            Center(
+                              child: Container(
+                                width: 350,
+                                height: 600,
+                                margin: const EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                      color: Colors.lightBlue, width: 4),
+                                ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Padding(
+                                      padding: EdgeInsets.all(40),
+                                      // child: Image.asset('assets/swipe_icon.png', width: 100, height: 100),
+                                      child: Icon(
+                                        Icons.swipe,
+                                        size: 100,
+                                        color: Colors.lightBlue,
+                                      ),
+                                    ),
+                                    Text(
+                                      // '#DeepQ',
+                                      "${question['q_title']}",
+                                      style: const TextStyle(
+                                          color: Colors.lightBlue,
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 20),
+                                  ],
                                 ),
                               ),
-                              Text(
-                                '#DeepQ',
-                                style: TextStyle(
-                                    color: Colors.lightBlue,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                            ],
-                          ),
+                            ),
+                          ],
+                        );
+
+                        // return Container(
+                        //   height: 200,
+                        //   width: double.infinity,
+                        //   decoration: const BoxDecoration(
+                        //     color: Colors.redAccent,
+                        //     borderRadius: BorderRadius.all(
+                        //       Radius.circular(10),
+                        //     ),
+                        //   ),
+                        //   child: const ClipRRect(),
+                        // );
+                      },
+                    ),
+                  ),
+                  // Expanded(
+                  //   child: Stack(
+                  //     children: [
+                  //       Positioned(
+                  //         top: 16,
+                  //         left: 16,
+                  //         child: Container(
+                  //           width: 40,
+                  //           height: 20,
+                  //           decoration: BoxDecoration(
+                  //             color: Colors.white.withOpacity(0.3),
+                  //             borderRadius: BorderRadius.circular(10),
+                  //           ),
+                  //         ),
+                  //       ),
+                  //       Center(
+                  //         child: Container(
+                  //           margin: const EdgeInsets.all(16),
+                  //           decoration: BoxDecoration(
+                  //             color: Colors.white,
+                  //             borderRadius: BorderRadius.circular(20),
+                  //             border: Border.all(color: Colors.lightBlue, width: 4),
+                  //           ),
+                  //           child: const Column(
+                  //             mainAxisSize: MainAxisSize.min,
+                  //             children: [
+                  //               Padding(
+                  //                 padding: EdgeInsets.all(40),
+                  //                 // child: Image.asset('assets/swipe_icon.png', width: 100, height: 100),
+                  //                 child: Icon(
+                  //                   Icons.swipe,
+                  //                   size: 100,
+                  //                   color: Colors.lightBlue,
+                  //                 ),
+                  //               ),
+                  //               Text(
+                  //                 '#DeepQ',
+                  //                 style: TextStyle(
+                  //                     color: Colors.lightBlue,
+                  //                     fontSize: 24,
+                  //                     fontWeight: FontWeight.bold),
+                  //               ),
+                  //               SizedBox(height: 20),
+                  //             ],
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SubscriptionScreen()),
+                      );
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFFF1493),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
                         ),
                       ),
-                    ],
-                  );
-
-                  // return Container(
-                  //   height: 200,
-                  //   width: double.infinity,
-                  //   decoration: const BoxDecoration(
-                  //     color: Colors.redAccent,
-                  //     borderRadius: BorderRadius.all(
-                  //       Radius.circular(10),
-                  //     ),
-                  //   ),
-                  //   child: const ClipRRect(),
-                  // );
-                },
-              ),
-            ),
-            // Expanded(
-            //   child: Stack(
-            //     children: [
-            //       Positioned(
-            //         top: 16,
-            //         left: 16,
-            //         child: Container(
-            //           width: 40,
-            //           height: 20,
-            //           decoration: BoxDecoration(
-            //             color: Colors.white.withOpacity(0.3),
-            //             borderRadius: BorderRadius.circular(10),
-            //           ),
-            //         ),
-            //       ),
-            //       Center(
-            //         child: Container(
-            //           margin: const EdgeInsets.all(16),
-            //           decoration: BoxDecoration(
-            //             color: Colors.white,
-            //             borderRadius: BorderRadius.circular(20),
-            //             border: Border.all(color: Colors.lightBlue, width: 4),
-            //           ),
-            //           child: const Column(
-            //             mainAxisSize: MainAxisSize.min,
-            //             children: [
-            //               Padding(
-            //                 padding: EdgeInsets.all(40),
-            //                 // child: Image.asset('assets/swipe_icon.png', width: 100, height: 100),
-            //                 child: Icon(
-            //                   Icons.swipe,
-            //                   size: 100,
-            //                   color: Colors.lightBlue,
-            //                 ),
-            //               ),
-            //               Text(
-            //                 '#DeepQ',
-            //                 style: TextStyle(
-            //                     color: Colors.lightBlue,
-            //                     fontSize: 24,
-            //                     fontWeight: FontWeight.bold),
-            //               ),
-            //               SizedBox(height: 20),
-            //             ],
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SubscriptionScreen()),
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                decoration: const BoxDecoration(
-                  color: Color(0xFFFF1493),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Try all packs!',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Try all packs!',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.keyboard_arrow_up, color: Colors.white),
+                        ],
+                      ),
                     ),
-                    SizedBox(width: 10),
-                    Icon(Icons.keyboard_arrow_up, color: Colors.white),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
